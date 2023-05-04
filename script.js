158a529a526028bb5b82feb891fb1aa215a70443
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-05-04 10:31:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 var angle = 0;
 		setInterval(function() {
 			angle += 1;
-			document.querySelector('.line').style.transform = 'rotate(' + angle + 'deg)';
+			document.querySelector('#line').style.transform = 'rotate(' + angle + 'deg)';
 		}, 10);
